8c75511ee0e7eb54eadb1d699707ccf561abff61
Harsha Roy <sreeharsha2133@gmail.com>
2026-01-31 10:44:15 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://HarshaRoy-133.github.io/Real_project4"
+  base:"/Real_project4/",
 })
